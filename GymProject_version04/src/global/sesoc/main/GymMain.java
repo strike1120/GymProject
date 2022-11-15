ceb939f3e3c709f909a6b4af0dbb631d5a57e7e5
CHO SUNGHO <70555626+strike1120@users.noreply.github.com>
2022-11-15 11:57:50 +0900

    Gym pjr v4

---
+++ GymProject_version04/src/global/sesoc/main/GymMain.java
@@ -0,0 +1,12 @@
+package global.sesoc.main;
+import global.sesoc.ui.GymUI;
+
+//사실상 service를 실행시키는 거 외에 하는일이 없음
+public class GymMain {
+
+	public static void main(String[] args) {
+		 new GymUI();
+		
+		
+	}
+}
